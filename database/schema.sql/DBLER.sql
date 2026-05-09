d80a4d8b15d9cd4cc85a5f7633f8c6ea8fd6c484
Abdurrahmansamli001 <abdurrahmansamli001@gmail.com>
2026-05-09 20:46:02 +0300

    initial commit

---
+++ database/schema.sql/DBLER.sql
@@ -0,0 +1,55 @@
+###CREATE TABLE user_custmer (
+   ###id INT AUTO_INCREMENT PRIMARY KEY,  
+   ###title varchar (100) ,
+   ###users varchar(50) ###
+   
+  ### SELECT * FROM user_custmer ;
+   
+   ###INSERT INTO user_custmer (id, title, users)
+   ###values ("2",'arkdas sikintili', 'ali yerli kaya')
+   
+   
+###SELECT * FROM users_custmer
+###WHERE users = 'ali yerli kaya';
+
+
+###SELECT * FROM user_custmer
+###WHERE users = 'ali yerli kaya';
+###DROP TABLE user_custmer;
+
+
+###NEW PROJECT
+
+###CREATE table users(
+###user varchar(50)
+###);
+
+###select * from users;
+
+###drop table users
+
+
+###select * from users;
+
+###create table movies (
+###id int primary key auto_increment,
+###title varchar(100) not null, 
+###description  TEXT,
+###catagory varchar(50),
+###year int
+###);
+
+
+select * from movies;
+
+
+###select * from users;
+
+
+###insert into users (id , username , password )
+####value ("1" , 'abdurrahman samli' , ' 411382092' )
+
+
+
+###insert into movies (id , title  , description , year )
+###value ( "1" , ' deneme ' , 'bilgilendirme' , 2001)
