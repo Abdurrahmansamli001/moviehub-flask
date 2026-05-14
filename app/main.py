d1ad6c03144c585dc41beae317bf5a2af58ce4cb
Abdurrahmansamli001 <abdurrahmansamli001@gmail.com>
2026-05-14 05:38:44 +0300

    v.s3

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import mysql.connector
 from app import config
 
@@ -17,14 +17,73 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 
-@app.route("/")
+@app.route("/", methods=["GET"]) 
 def home():
-    return "server is running"
+    query = "SELECT * FROM movies"
+    cursor.execute(query)
+    movies = cursor.fetchall()
+    return render_template("index.html", movies=movies)
 
 
-@app.route("/add-user")
+
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    try:
+        username =request.form.get("username")
+        password=request.form.get("password")
+
+        if username and password:
+
+            query = "SELECT * FROM users WHERE username = %s AND password = %s"
+            cursor.execute(query,(username, password))
+            user = cursor.fetchone()
+
+            if user : 
+                return{"status": "success", "message": "login successfull"}
+            else:
+                return {"status": "failed", "message": "invalid credentials"}, 401
+        else:
+            return{"status": "failed", "message" : " username or  password missing"}, 400
+          
+    except mysql.connector.Error as db_error:
+        return {"eror": "database error"}, 500
+    except Exception as e:
+        return{"error": "Unknown error"}, 400
+
+
+
+
+@app.route("/add-user", methods=["POST"] )
 def add_user():
-    return ("true")
+    try:
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username and password:
+            query = "INSERT INTO users (username , password) VALUES (%s , %s)"
+            cursor.execute(query,  (username, password))
+            db.commit()
+            
+
+            return {"status": "success", "message": "user created"}
+        else:
+            return{"status": "failed", "message": "missing fields"}, 400
+        
+
+
+
+    except mysql.connector.Error as db_error:
+        return {"error": "database error"}, 500
+    except Exception as e:
+        return {"erorr": "Unknown error"}, 400    
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
